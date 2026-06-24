345538374252f90a518450eab24fa6f45a10e95f
dotslashapaar <apaarsaini007@gmail.com>
2026-06-24 16:34:01 +0530

    added model

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use bookmarks_api::{db, error, handlers, models, routes};
+use std::env;
 
-fn main() {
-    println!("Hello, world!");
+use bookmarks_api::{db, error, handlers, models, routes};
+use dotenvy::dotenv;
+use sqlx::PgPool;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPool::connect(&database_url).await?;
+    let row: (i32,) = sqlx::query_as("SELECT 1").fetch_one(&pool).await?;
+    println!("db says {}", row.0);
+    
+
+    Ok(())
 }
--- src/models/bookmark.rs
+++ src/models/bookmark.rs
@@ -1,1 +1,38 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Bookmark {
+    pub id: Uuid,
+    pub owner_id: Uuid,
+    pub title: String,
+    pub url: String,
+    pub description: Option<String>,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct User {
+    pub id: Uuid,
+    pub password_hash: String,
+    pub email: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateBookmark {
+    pub title: String,
+    pub url: String,
+    pub description: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateBookmark {
+    pub title: Option<String>,
+    pub url: Option<String>,
+    pub description: Option<String>,
+}
 
