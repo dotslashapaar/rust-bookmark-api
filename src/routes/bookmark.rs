d55efb667abff8013872bb4db22f92d02c09e173
dotslashapaar <apaarsaini007@gmail.com>
2026-06-26 12:25:17 +0530

    added routes

--- src/routes/bookmark.rs
+++ src/routes/bookmark.rs
@@ -1,1 +1,28 @@
+use axum::{
+    Router,
+    routing::{delete, get, patch, post},
+};
 
+use crate::handlers::bookmark::{
+    AppState, create_bookmark, delete_bookmark, get_bookmark, list_all_bookmarks, update_bookmark,
+};
+
+pub fn bookmark_routes() -> Router<AppState> {
+    Router::new()
+        .route("/bookmarks", get(list_all_bookmarks))
+        .route("/bookmarks", post(create_bookmark))
+        .route("/bookmarks/{id}", get(get_bookmark))
+        .route("/bookmarks/{id}", patch(update_bookmark))
+        .route("/bookmarks/{id}", delete(delete_bookmark))
+}
+
+// pub fn bookmark_routes() -> Router<AppState> {
+//     Router::new()
+//         .route("/bookmarks", get(list_all_bookmarks).post(create_bookmark))
+//         .route(
+//             "/bookmarks/{id}",
+//             get(get_bookmark)
+//                 .patch(update_bookmark)
+//                 .delete(delete_bookmark),
+//         )
+// }
