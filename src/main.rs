1cdc063943bfefb7945090a7ee2b3436449d4ac9
dotslashapaar <apaarsaini007@gmail.com>
2026-06-26 12:32:28 +0530

    updated main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
 
-use bookmarks_api::{db, error, handlers, models, routes};
+use bookmarks_api::{
+    db::bookmark::BookmarkRepo, handlers::bookmark::AppState, routes::bookmark::bookmark_routes,
+};
 use dotenvy::dotenv;
 use sqlx::PgPool;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -10,8 +13,17 @@ async fn main() -> anyhow::Result<()> {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPool::connect(&database_url).await?;
-    let row: (i32,) = sqlx::query_as("SELECT 1").fetch_one(&pool).await?;
-    println!("db says {}", row.0);
+
+    let repo = BookmarkRepo::new(pool);
+    let state = AppState {
+        bookmark_repo: repo,
+    };
+
+    let app = bookmark_routes().with_state(state);
+
+    let listner = TcpListener::bind("0.0.0.0:3000").await?;
+    println!("Listening on {}", listner.local_addr()?);
+    axum::serve(listner, app).await?;
 
     Ok(())
 }
